faf4ec08ee2dd8213c00391cf402ab57c3e768f5
Chatbrume <nioche.amelie@gmail.com>
2022-04-07 15:43:17 +0200

    Ajout swagger et findByType

--- src/main/java/eu/ensup/manageraccount/ManagerAccountApplication.java
+++ src/main/java/eu/ensup/manageraccount/ManagerAccountApplication.java
@@ -1,11 +1,22 @@
 package eu.ensup.manageraccount;
 
+import eu.ensup.manageraccount.entity.Account;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class ManagerAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerAccountApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(RepositoryRestConfiguration restConfiguration){
+        return args -> {
+            restConfiguration.exposeIdsFor(Account.class);
+        };
     }
 }
--- src/main/java/eu/ensup/manageraccount/repository/AccountRepository.java
+++ src/main/java/eu/ensup/manageraccount/repository/AccountRepository.java
@@ -1,9 +1,15 @@
 package eu.ensup.manageraccount.repository;
 
 import eu.ensup.manageraccount.entity.Account;
+import eu.ensup.manageraccount.entity.TypeAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
-@RestResource
+import java.util.List;
+
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    List<Account> findByType(TypeAccount type);
 }
